dd2898aa0aa7e64e1d5ac34f212c768bd0d10dbc
codlab <codlabtech@gmail.com>
2018-02-08 14:38:16 +0100

    add theme

---
+++ src/Theme.js
@@ -0,0 +1,15 @@
+const COLOR_PRIMARY = "#2196F3";
+
+const UI_THEME = {
+  palette: {
+    primaryColor: ColorPrimary,
+    accentColor: ColorPrimary
+  },
+  toolbar: {
+      container: {
+          height: 56
+      }
+  }
+};
+
+exports default UI_THEME;
