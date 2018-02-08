3ff3485dac1f4baabc45c6f186229edb3b400bc1
codlab <codlabtech@gmail.com>
2018-02-08 14:52:01 +0100

    add main screen's toolbar

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -7,18 +7,23 @@
 import React, { Component } from 'react';
 import {
   Text,
-  View
+  View,
+  StatusBar
 } from 'react-native';
+import { Toolbar, ThemeProvider } from "react-native-material-ui";
 
 import MainList from './MainList';
 import styles from "./Styles";
+import Theme from "./Theme";
 
 type Props = {};
 export default class MainScreen extends Component<Props> {
 
   state = {
     contact: null
-  }
+  };
+
+  empty = <View />;
 
   onContactClick(contact) {
     console.warn(contact);
@@ -30,18 +35,31 @@ export default class MainScreen extends Component<Props> {
 
   render() {
     const { contact } = this.state;
+    const has_contact = contact != null;
 
-    if (contact) {
-      return (
-        <View style={styles.mainScreenContainer}>
-          <MainList style={styles.mainScreenHalfScreen} onContactClick={(c) => this.onContactClick(c)}/>
-          <Text style={styles.mainScreenHalfScreen}>{contact.jobTitle}</Text>
-        </View>
-      );
-    } else {
-      return (
-        <MainList onContactClick={(c) => this.onContactClick(c)}/>
-      );
-    }
+    return (
+      <ThemeProvider key="one" uiTheme={Theme} children={this.empty}>
+        <StatusBar backgroundColor="rgba(0, 0, 0, 0.2)" translucent />
+        <View style={{ backgroundColor: Theme.palette.primaryColor, height: 24 }} />
+        <Toolbar
+          leftElement={has_contact ? "arrow-back" : ""}
+          onLeftElementPress={() => {}}
+          centerElement="ContactManager"/>
+        {
+          has_contact && (
+            <View style={styles.mainScreenContainer}>
+              <MainList style={styles.mainScreenHalfScreen} onContactClick={(c) => this.onContactClick(c)}/>
+              <Text style={styles.mainScreenHalfScreen}>{contact.jobTitle}</Text>
+            </View>
+          )
+        }
+
+        {
+          (!has_contact) && (
+            <MainList onContactClick={(c) => this.onContactClick(c)}/>
+          )
+        }
+      </ThemeProvider>
+    )
   }
 }
